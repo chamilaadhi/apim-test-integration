cdaf5043501119d909b931fd8af1f3cfcf6c1dc7
Chamila Adhikarinayake <cka.adhi@gmail.com>
2023-07-13 12:06:59 +0530

    Update deploy-cluster.sh

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -3,11 +3,13 @@
 
 echo "Hello world" $1 $2
 
-
+echo pwd
 pass=$1
 
 echo "pass $pass" 
 
 ls
 
-helm install wso2am advanced/am-pattern-2 --version 3.2.0-5 --namespace wso2 --dependency-update --create-namespace --set wso2.subscription.username=$1 --set wso2.subscription.password=$2 --set wso2.u2.username=$1 --set wso2.u2.password=$2
+ls kubernetes
+
+helm install wso2am kubernetes/advanced/am-pattern-2 --version 3.2.0-5 --namespace wso2 --dependency-update --create-namespace --set wso2.subscription.username=$1 --set wso2.subscription.password=$2 --set wso2.u2.username=$1 --set wso2.u2.password=$2
