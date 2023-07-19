f8f5090e168ad30336ac2c523188f472891a4003
Chamila Adhikarinayake <cka.adhi@gmail.com>
2023-07-19 19:01:45 +0530

    fix

--- run-test.sh
+++ run-test.sh
@@ -3,8 +3,8 @@
 currentDir="$PWD"
 echo "currentDir:" $currentDir
 
-collection_file=scripts/tests-cases/profile-tests/APIM_Environment.postman_environment.json
-environment_file=scripts/tests-cases/profile-tests/Profile_Setup_Tests.postman_collection.json
+collection_file=$currentDir/scripts/tests-cases/profile-tests/APIM_Environment.postman_environment.json
+environment_file=$currentDir/scripts/tests-cases/profile-tests/Profile_Setup_Tests.postman_collection.json
 ip=$(kubectl -n ingress-nginx get svc ingress-nginx-controller -o json | jq ".status.loadBalancer.ingress[0].ip")
 echo "ip:" $ip
 
