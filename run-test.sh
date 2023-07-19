84e02d9dcc286d16878b98e5991474f604fb9281
Chamila Adhikarinayake <cka.adhi@gmail.com>
2023-07-19 18:44:37 +0530

    fix

--- run-test.sh
+++ run-test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-collection_file=tests-cases/profile-tests/APIM_Environment.postman_environment.json
-environment_file=tests-cases/profile-tests/Profile_Setup_Tests.postman_collection.json
+echo pwd
+
+collection_file=scripts/tests-cases/profile-tests/APIM_Environment.postman_environment.json
+environment_file=scripts/tests-cases/profile-tests/Profile_Setup_Tests.postman_collection.json
 ip=$(kubectl -n ingress-nginx get svc ingress-nginx-controller -o json | jq ".status.loadBalancer.ingress[0].ip")
+echo "ip:" $ip
 
 /var/lib/jenkins/node-v18.17.0-linux-x64/bin/newman run "$collection_file" \
   --environment "$environment_file" \
