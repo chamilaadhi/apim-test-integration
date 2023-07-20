e7901d0f22097d9fb26ed920a9a9892d1a3e15cd
Chamila Adhikarinayake <cka.adhi@gmail.com>
2023-07-20 14:42:53 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -92,7 +92,7 @@ product_name=apim
 collection_file=kubernetes/product-deployment/scripts/${product_name}/test-${product_name}/tests-cases/profile-tests/Profile_Setup_Tests.postman_collection.json
 environment_file=kubernetes/product-deployment/scripts/${product_name}/test-${product_name}/tests-cases/profile-tests/APIM_Environment.postman_environment.json
 echo "==== Running newman tests == "
-newman run "$collection_file" \
+/home/ubuntu/.nvm/versions/node/v19.0.1/bin/newman run "$collection_file" \
   --environment "$environment_file" \
   --env-var "cluster_ip=${HOST_NAME}" \
   --insecure
