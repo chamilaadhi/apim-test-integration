f1c775da76f81d712fec920ada4db29e9d835ef6
Chamila Adhikarinayake <cka.adhi@gmail.com>
2023-07-26 11:51:09 +0530

    Update deploy-cluster.sh

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -12,7 +12,7 @@ ls
 
 ls scripts/kubernetes
 
-#helm install wso2am scripts/kubernetes/advanced/am-pattern-2 --version 3.2.0-5 --namespace wso2 --dependency-update --create-namespace --set wso2.subscription.username=$1 --set wso2.subscription.password=$2 --set wso2.u2.username=$1 --set wso2.u2.password=$2
+helm install wso2am scripts/kubernetes/advanced/am-pattern-2 --version 3.2.0-5 --namespace wso2 --dependency-update --create-namespace --set wso2.subscription.username=$1 --set wso2.subscription.password=$2 --set wso2.u2.username=$1 --set wso2.u2.password=$2
 
 
 # Reverse the variable content
