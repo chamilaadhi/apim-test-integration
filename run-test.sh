97d057bc7c88370e37855ebfb579fe164b99811a
Chamila Adhikarinayake <cka.adhi@gmail.com>
2023-07-29 11:09:42 +0530

    test

--- run-test.sh
+++ run-test.sh
@@ -2,12 +2,29 @@
 
 currentDir="$PWD"
 echo "currentDir:" $currentDir
+reldir=`dirname $0`
 
-collection_file=$currentDir/scripts/tests-cases/profile-tests/APIM_Environment.postman_environment.json
-environment_file=$currentDir/scripts/tests-cases/profile-tests/Profile_Setup_Tests.postman_collection.json
 ip=$(kubectl -n ingress-nginx get svc ingress-nginx-controller -o json | jq ".status.loadBalancer.ingress[0].ip")
 echo "ip:" $ip
 
-newman run
-echo '/var/lib/jenkins/node-v18.17.0-linux-x64/bin/newman run "$collection_file" --environment "$environment_file" --env-var "cluster_ip=$ip" --insecure'
-/var/lib/jenkins/node-v18.17.0-linux-x64/bin/newman run "$collection_file" --environment "$environment_file" --env-var "cluster_ip=$ip" --insecure
+analytics_collection_file=$reldir/tests-cases/analytics-tests/analytics_test.json
+analytics_environment_file=$reldir/tests-cases/analytics-tests/analytics_environment.json
+ 
+newman run "$analytics_collection_file" \
+  --environment "$analytics_environment_file" \
+  --env-var "cluster_ip=$ip" \
+  --insecure \
+  --reporters cli,junit \
+  --reporter-junit-export newman-analytics-results.xml
+
+
+collection_file=$reldir/scripts/tests-cases/profile-tests/APIM_Environment.postman_environment.json
+environment_file=$reldir/scripts/tests-cases/profile-tests/Profile_Setup_Tests.postman_collection.json
+
+
+newman run "$collection_file" \
+   --environment "$environment_file" \
+   --env-var "cluster_ip=$ip"\
+   --insecure \
+   --reporters cli,junit \
+   --reporter-junit-export newman-profile-results.xml
