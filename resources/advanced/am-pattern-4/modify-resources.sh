d691cf6488cb184997f16a7faa80d03a7e9005cf
Chamila Adhikarinayake <cka.adhi@gmail.com>
2023-08-03 12:21:59 +0530

    Add resource modification

--- resources/advanced/am-pattern-4/modify-resources.sh
+++ resources/advanced/am-pattern-4/modify-resources.sh
@@ -13,3 +13,5 @@ cp -r $reldir/* $k8s_repo_dir/advanced/am-pattern-4/
 
 sed -i '/type = "database_unique_id"/a    \\n    [oauth.endpoints]\n    oauth2_jwks_url="https://{{ template "am-pattern-4.resource.prefix" . }}-am-cp-service:${mgt.transport.https.port}/oauth2/jwks"' $k8s_repo_dir/advanced/am-pattern-4/templates/am/control-plane/instance-1/wso2am-pattern-4-am-control-plane-conf.yaml
 sed -i '/type = "database_unique_id"/a    \\n    [oauth.endpoints]\n    oauth2_jwks_url="https://{{ template "am-pattern-4.resource.prefix" . }}-am-cp-service:${mgt.transport.https.port}/oauth2/jwks"' $k8s_repo_dir/advanced/am-pattern-4/templates/am/control-plane/instance-2/wso2am-pattern-4-am-control-plane-conf.yaml
+
+sed -i '/paths:/a \      - path: /api/am/gateway/v2/\n        pathType: Prefix\n        backend:\n          service:\n            name: {{ template "am-pattern-4.resource.prefix" . }}-am-gateway-service\n            port:\n              number: 9443' $k8s_repo_dir/advanced/am-pattern-4/templates/am/gateway/wso2am-pattern-4-am-gateway-ingress.yaml
